229b4d32945441f2ad011d314b43f2f7e5f6d16f
Sergio J Cerezo <sergio.cerezo@accedo.tv>
2023-09-25 12:45:37 -0600

    Trying to trigger CI/CD

--- src/controllers/base-controller.js
+++ src/controllers/base-controller.js
@@ -4,7 +4,7 @@ module.exports = {
     index: (req, res) => {
         return res.status(200).json({
             success: true,
-            message: "Hey there 👋🏾",
+            message: "Hey there 👋🏾, ver 1.1",
         })
     },
 
